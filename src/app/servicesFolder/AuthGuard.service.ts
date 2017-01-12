af55840dbb2286496e2dfc023c527fadd9603e01
v2krishalsinhraj <krishalsinh.raj@v2solutions.com>
2017-01-12 20:57:52 +0530

    Time zone fix for US employee

--- src/app/servicesFolder/AuthGuard.service.ts
+++ src/app/servicesFolder/AuthGuard.service.ts
@@ -10,18 +10,25 @@ export class AuthGuard implements CanActivate {
 
         return navigator.permissions.query({ name: 'geolocation' }).then(function (PermissionStatus) {
             PermissionStatus.state == 'granted' ? true : false; // prompt, granted, denied
+            let profileLocation = JSON.parse(localStorage.getItem("profile")).ol;
 
             PermissionStatus.onchange = function () {
-                if (PermissionStatus.state != 'granted') {
-                    window.location.href = '/vibranthelp/help-location.html';
+                if (profileLocation == 2) {
+                    if (PermissionStatus.state != 'granted') {
+                        window.location.href = '/vibranthelp/help-location.html';
+                    }
                 }
             }
-
-            if (PermissionStatus.state == 'granted') {
-                return true;
+            if (profileLocation == 2) {
+                if (PermissionStatus.state == 'granted') {
+                    return true;
+                }
+                else {
+                    return false;
+                }
             }
             else {
-                return false;
+                return true;
             }
         })
     }
